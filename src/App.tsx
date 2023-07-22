a211b1f52ebee44fbbe691f759d0dfe54209bf34
Paulo Henrique Bomfim <paulo.bomfim@lekto.com.br>
2023-07-22 01:03:06 -0300

    :wrench: chore: setup eslint and define global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './global'
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello world</h1>
-    </>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
